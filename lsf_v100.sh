7bc484ddff21c9865f63437c7c62dea66c33c53d
Duong Tran <d@dttt.io>
2024-03-04 16:44:38 +0100

    1st commit

--- lsf_v100.sh
+++ lsf_v100.sh
@@ -1,4 +1,4 @@
-#BSUB -J Owl[1-2]                 # Specify the job name
+#BSUB -J Owl[1-5]                 # Specify the job name
 #BSUB -W 168:00                # Specify the maximum runtime in "hours:minutes"
 #BSUB -o %x.%j.out            # Determine where the output will be written
 #BSUB -e %x.%j.err            # The same goes for the error file
