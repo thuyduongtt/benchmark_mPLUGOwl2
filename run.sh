e923c01150a0e1c92344de99c40c30cabeba077e
Duong Tran <d@dttt.io>
2024-03-04 16:43:49 +0100

    1st commit

--- run.sh
+++ run.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
 
+LIMIT=40
+
 case $1 in
   1)
     DS_NAME="unbalanced"
+    START=0
     ;;
   2)
-    DS_NAME="balanced_10"
+    DS_NAME="unbalanced"
+    START=40
+    ;;
+  3)
+    DS_NAME="unbalanced"
+    START=80
+    ;;
+  4)
+    DS_NAME="unbalanced"
+    START=120
+    ;;
+  5)
+    DS_NAME="unbalanced"
+    START=160
     ;;
 esac
 
 DS_DIR="../dataset/${DS_NAME}"
 python start.py \
  --path_to_ds $DS_DIR \
- --output_dir_name output_${DS_NAME}
+ --output_dir_name output_${DS_NAME}_${START} \
+ --start_at $START \
+ --limit $LIMIT
 
 python start.py \
  --path_to_ds $DS_DIR \
- --output_dir_name output_${DS_NAME}_test \
- --split test
+ --output_dir_name output_${DS_NAME}_test_${START} \
+ --split test \
+ --start_at $START \
+ --limit $LIMIT
