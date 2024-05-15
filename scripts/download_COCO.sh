9b5b7a3d253b0a5c9f0067e6a1d33f87aec9b4d9
Duong Tran <d@dttt.io>
2024-05-15 23:00:27 +0200

    * More datasets

--- scripts/download_COCO.sh
+++ scripts/download_COCO.sh
@@ -3,6 +3,8 @@
 cd ../../dataset
 
 # COCO val
+mkdir COCO
+cd COCO
 wget http://images.cocodataset.org/zips/val2014.zip
 unzip -q val2014.zip
 rm val2014.zip
--- scripts/download_OKVQA.sh
+++ scripts/download_OKVQA.sh
@@ -5,10 +5,25 @@ cd ../../dataset
 # OKVQA
 mkdir OKVQA
 cd OKVQA
-wget https://okvqa.allenai.org/static/data/mscoco_val2014_annotations.json.zip
-unzip -q mscoco_val2014_annotations.json.zip
-rm mscoco_val2014_annotations.json.zip
-wget https://okvqa.allenai.org/static/data/OpenEnded_mscoco_val2014_questions.json.zip
-unzip -q OpenEnded_mscoco_val2014_questions.json.zip
-rm OpenEnded_mscoco_val2014_questions.json.zip
 
+#wget https://okvqa.allenai.org/static/data/mscoco_val2014_annotations.json.zip
+#unzip -q mscoco_val2014_annotations.json.zip
+#rm mscoco_val2014_annotations.json.zip
+#wget https://okvqa.allenai.org/static/data/OpenEnded_mscoco_val2014_questions.json.zip
+#unzip -q OpenEnded_mscoco_val2014_questions.json.zip
+#rm OpenEnded_mscoco_val2014_questions.json.zip
+
+# Install gdown
+pip install --upgrade --no-cache-dir gdown
+
+ZIP_ID='1Rro5L20br0acFFp-ZY-QVxN9zm0-CkgI'
+ZIP_NAME='mscoco_val2014_annotations.json'
+gdown $ZIP_ID -O $ZIP_NAME
+unzip -q $ZIP_NAME
+rm $ZIP_NAME
+
+ZIP_ID='1m9LyAV-uHkGroHaFvIsbaaVWdKXN0zHs'
+ZIP_NAME='OpenEnded_mscoco_val2014_questions.json'
+gdown $ZIP_ID -O $ZIP_NAME
+unzip -q $ZIP_NAME
+rm $ZIP_NAME
