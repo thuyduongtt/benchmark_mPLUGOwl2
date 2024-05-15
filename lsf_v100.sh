22fd6404f839e019921b5ad749d20c0b7bde43ef
Duong Tran <d@dttt.io>
2024-05-15 22:32:54 +0200

    * More datasets

--- lsf_v100.sh
+++ lsf_v100.sh
@@ -1,4 +1,4 @@
-#BSUB -J Owl[1-7]                 # Specify the job name
+#BSUB -J Owl               # Specify the job name
 #BSUB -W 168:00                # Specify the maximum runtime in "hours:minutes"
 #BSUB -o %x.%j.out            # Determine where the output will be written
 #BSUB -e %x.%j.err            # The same goes for the error file
